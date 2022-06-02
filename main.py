5572fd4852e92dc1dcb340be6b2f008e353998c9
nshadybekov <shadybekov@cointelegraph.com>
2022-06-02 11:41:32 +0300

    bubble sort

---
+++ main.py
@@ -0,0 +1,12 @@
+import bubble_sort
+
+
+def bubbles():
+    l = [6, 8, 1, 4, 10, 7, 8, 9, 3, 2, 5]
+    print(f"Bubble sort array is:{l}")
+    bubble_sort.bubble_sort(l)
+    print(f"Bubble sorted array is:{l}")
+
+
+if __name__ == '__main__':
+    bubbles()
